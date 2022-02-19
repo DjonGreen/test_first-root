83bae2f690e91aa367937cc8d5e8d28c7bb1e13d
DjonGreen <djongreen94@gmail.com>
2022-02-19 11:58:40 +0300

    Add function lenght text

--- test_first/views.py
+++ test_first/views.py
@@ -13,5 +13,6 @@ def feedback(request):
 def reverse(request):
     user_text = request.GET['usertext']
     print(user_text)
+    len_usertext = len(user_text.split())
     reverse_user_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_user_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_user_text, 'lentext': len_usertext})
